5365066f1be63ba8f96dbf0f6aae4f04603f46fc
Wren Liang <wren.liang@icloud.com>
2019-03-08 11:56:14 -0800

    Initial Commit

---
+++ Bluetooth draft mk1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bluetooth draft mk1
+//
+//  Created by Wren Liang on 2019-03-08.
+//  Copyright © 2019 Wren Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
